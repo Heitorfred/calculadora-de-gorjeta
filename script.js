490fccc8dca6ff9248c8275a9c86feff608b3b47
heitor girola <heitorfrederico009@gmail.com>
2026-06-01 02:10:35 -0300

    segundo commit de Js

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ let porcentagem =
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
 
-function receberValorConta(evento) {
-    conta = Number(evento.target.value)
-}
-
-const pessoasInput = document.querySelector("#pessoas")
-pessoasInput.addEventListener("input", receberQuantidadePessoas)
-
 function receberQuantidadePessoas(evento) {
     const paragrafoErro = document.querySelector(".pessoas #erro")
     const divErro = document.querySelector(".pessoas .input-box")
@@ -28,4 +21,14 @@ function receberQuantidadePessoas(evento) {
     }
 }
 
+
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click", receberPorcentagem)
+})
+
+
+function receberPorcentagem(evento) {
+    console.log(evento.target.value)
+}
 
